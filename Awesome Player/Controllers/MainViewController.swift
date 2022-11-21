82b59554751f673dc03ee0e6218024ae6cdb6793
Vyucheiski Vitaliy <vyucheiski@scand.com>
2022-11-21 05:35:34 +0300

    Minor change in code structure

--- Awesome Player/Controllers/MainViewController.swift
+++ Awesome Player/Controllers/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 // 
 
+import SnapKit
 import UIKit
 
 class MainViewController: UIViewController {
@@ -33,19 +34,6 @@ class MainViewController: UIViewController {
         return button
     }()
 
-    private lazy var disconnectButton: UIButton = {
-        let button = UIButton()
-		button.backgroundColor = UIColor(asset: Asset.red)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.contentEdgeInsets = UIEdgeInsets(top: 11.75, left: 32.0, bottom: 11.75, right: 32.0)
-        button.layer.cornerRadius = 20.0
-		button.setTitle(L10n.disconnect, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        button.sizeToFit()
-        button.addTarget(self, action: #selector(didTapSignOut(_: )), for: .touchUpInside)
-        return button
-    }()
-
     // MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,24 +43,20 @@ class MainViewController: UIViewController {
     // MARK: Methods
     func setupViews() {
         view.backgroundColor = UIColor.white
+
         view.addSubview(connectLabel)
         view.addSubview(connectButton)
-        view.addSubview(disconnectButton)
-        let constant: CGFloat = 16.0
+
+		let constant: CGFloat = 16.0
+
         connectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         connectButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        disconnectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        disconnectButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
         connectLabel.centerXAnchor.constraint(equalTo: connectButton.centerXAnchor).isActive = true
         connectLabel.bottomAnchor.constraint(equalTo: connectButton.topAnchor, constant: -constant).isActive = true
-        connectButton.sizeToFit()
-        disconnectButton.sizeToFit()
     }
 
     @objc
-	func didTapConnect(
-		_ button: UIButton
-	) {
+	func didTapConnect(_ button: UIButton) {
         AuthManager().didTapConnect()
     }
 
